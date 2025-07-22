0e62fbd0df47da3012f24590a4f0cef26e8ff8b3
Ronak kudal <ronakkudal00@gmail.com>
2025-07-22 17:33:47 +0530

    Adding the features section and platform highlights

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 
 import Navbar from "./components/Navbar";
 import CampusHero from "./components/CampusHero";
+//import FeaturesSections from "./components/FeaturesSections/";
+
 import Hero from "./components/Hero";
+import PlatformHighlights from "./components/PlatformHighlights";
 //import Carousel from "./components/Carousel";
 //import Footer from "./components/Footer";
 
@@ -10,6 +13,8 @@ export default function App() {
     <div className="bg-gradient-to-b from-gray-800 to-gray-900 min-h-screen min-w-screen text-white font-sans">
       <Navbar />
       <CampusHero />
+    {/* <FeaturesSections /> */}
+    <PlatformHighlights/>
       {/* <Hero /> */}
       
       {/* <Carousel />
