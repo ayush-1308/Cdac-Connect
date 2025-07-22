32d1bc697ceca5ecdcc4439fa89564351e34de1e
Ayush <ayushsinghlts123@gmail.com>
2025-07-22 19:42:42 +0530

    Adding React-Router and Structing the Home Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,14 @@
-
-import Navbar from "./components/Navbar";
-import CampusHero from "./components/CampusHero";
-//import FeaturesSections from "./components/FeaturesSections/";
-
-import Hero from "./components/Hero";
-import PlatformHighlights from "./components/PlatformHighlights";
-import FeaturedCampuses from "./components/FeaturedCampuses";
-import Footer from "./components/Footer";
-import { useRef } from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 export default function App() {
-  const footerRef = useRef(null);
-
-  const scrollToFooter = () => {
-    footerRef.current?.scrollIntoView({ behavior: "smooth" });
-  };
   return (
-    <div className="bg-gradient-to-b from-gray-800 to-gray-900 min-h-screen min-w-screen text-white font-sans">
-      <Navbar onContactClick={scrollToFooter} />
-      <CampusHero />
-      <PlatformHighlights/>
-      <FeaturedCampuses/>
-      <Footer ref={footerRef} /> 
-    </div>
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,27 @@
+import { useRef } from "react";
+import CampusHero from "./CampusHero";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import PlatformHighlights from "./PlatformHighlights";
+import FeaturedCampuses from "./FeaturedCampuses";
+
+const Home = () => {
+    const footerRef = useRef(null);
+    
+    const scrollToFooter = () => {
+        footerRef.current?.scrollIntoView({ behavior: "smooth" });
+    };
+  return (
+    <>
+      <div className="bg-gradient-to-b from-gray-800 to-gray-900 min-h-screen min-w-screen text-white font-sans overflow-y-scroll hide-scrollbar">
+      <Navbar onContactClick={scrollToFooter} />
+      <CampusHero />
+      <PlatformHighlights />
+      <FeaturedCampuses />
+      <Footer ref={footerRef} />
+    </div>
+    </>
+  );
+};
+
+export default Home;
