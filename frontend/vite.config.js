ca2be1cea9de6daa06d84e7e99b1223bafd47afd
Ayush <ayushsinghlts123@gmail.com>
2025-08-05 23:20:48 +0530

    adding more packages for build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,29 +1,60 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
-   define: {
-    global: 'globalThis'
+  plugins: [react(), tailwindcss()],
+  define: {
+    global: 'globalThis',
+    'process.env': {}, // Required by some polyfilled packages
   },
   resolve: {
-   alias: {
-      util: 'rollup-plugin-node-polyfills/polyfills/util',
-      process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
-    }
+    alias: {
+      // Add buffer alias explicitly
+      buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6.js',
+      process: 'rollup-plugin-node-polyfills/polyfills/process-es6.js',
+      util: 'rollup-plugin-node-polyfills/polyfills/util.js',
+    },
   },
   optimizeDeps: {
-    include: ['sockjs-client'],
+    include: ['buffer', 'process', 'util', 'sockjs-client'],
   },
   build: {
     rollupOptions: {
-      plugins: [rollupNodePolyFill()]
-    }
-  }
-})
+      plugins: [rollupNodePolyFill()],
+    },
+  },
+});
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),
+//     tailwindcss()
+//   ],
+//    define: {
+//     global: 'globalThis'
+//   },
+//   resolve: {
+//    alias: {
+//       util: 'rollup-plugin-node-polyfills/polyfills/util',
+//       process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
+//     }
+//   },
+//   optimizeDeps: {
+//     include: ['sockjs-client'],
+//   },
+//   build: {
+//     rollupOptions: {
+//       plugins: [rollupNodePolyFill()]
+//     }
+//   }
+// })
